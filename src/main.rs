924efc5c2d91207c012ccfa22ef2bc950297356c
Rark7040 <crimera.hello@gmail.com>
2021-12-10 09:55:32 +0900

    added vec

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,11 @@
 mod cast;
 mod array;
 mod hash_map;
+mod vec;
 
 fn main() {
     cast::cast();
     array::array();
     hash_map::hashMap();
+    vec::vec();
 }
---
+++ src/vec.rs
@@ -0,0 +1,5 @@
+pub fn vec(){
+    let mut v = vec![10, 40, 10];
+    v.push(10);
+    println!("{:?}", v);
+}
