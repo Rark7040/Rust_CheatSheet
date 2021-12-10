41e3c4db6c3dae67028c99a385b350865c7bd268
Rark7040 <crimera.hello@gmail.com>
2021-12-10 09:41:16 +0900

    update hashmap

--- src/hash_map.rs
+++ src/hash_map.rs
@@ -2,6 +2,9 @@ use std::collections::HashMap;
 
 pub fn hashMap(){
 	let mut map = HashMap::new();
-	map.insert("aaa".to_string(), 10);
+	map.insert("aaa", 10);
+	map.insert("bbb", 20);
+	map.insert("ccc", 30);
+	map.remove("bbb");
 	println!("{:?}", map);
 }
