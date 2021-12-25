fb2aed49dbfc62c644a60473fe78a1d64e363e48
Rark7040 <crimera.hello@gmail.com>
2021-12-25 10:43:13 +0900

    vec iterator

--- src/vec.rs
+++ src/vec.rs
@@ -1,5 +1,10 @@
 pub fn vec(){
     let mut v = vec![10, 40, 10];
-    v.push(10);
-    println!("{:?}", v);
+    v.push(30);
+    println!("Vec [");
+
+    for value in v{
+        println!("  {:?},", value);
+    }
+    println!("]");
 }
