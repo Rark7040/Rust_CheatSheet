bb52cbcb520137be3d0c924dc040410edb6e2d1d
Rark7040 <crimera.hello@gmail.com>
2021-12-28 16:31:06 +0900

    setter

--- src/_struct.rs
+++ src/_struct.rs
@@ -6,10 +6,19 @@ pub struct Vector3{
     z: i32
 }
 impl Vector3{
-    pub fn new(x: i32, y:i32, z:i32)->Vector3 {Vector3{x, y, z}}
+    pub fn new(x: i32, y: i32, z: i32)->Vector3 {Vector3{x, y, z}}
     pub fn _get_x(&self)->&i32 {&self.x}
     pub fn _get_y(&self)->&i32 {&self.y}
     pub fn _get_z(&self)->&i32 {&self.z}
+    pub fn _set_x(&mut self, new_x: i32){
+        self.x = new_x;
+    }
+    pub fn _set_y(&mut self, new_y: i32){
+        self.y = new_y;
+    }
+    pub fn _set_z(&mut self, new_z: i32){
+        self.z = new_z;
+    }
 }
 
 impl Debug for Vector3{
@@ -29,6 +38,7 @@ impl Drop for Vector3{
 }
 
 pub fn _struct(){
-    let v = Vector3::new(10, 30, 10);
+    let mut v = Vector3::new(10, 30, 10);
+    v._set_x(50);
     println!("{:?}", v);
 }
